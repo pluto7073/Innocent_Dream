7d0394fc9e460ef1014900796124fe3a4aeca987
Pluto7073 <20planetpluto05@gmail.com>
2023-02-24 21:35:42 -0500

    Fixed re-initialization problem for THIS class

--- src/java/io/innocentdream/objects/TexturedObject.java
+++ src/java/io/innocentdream/objects/TexturedObject.java
@@ -9,10 +9,12 @@ import java.awt.*;
 public abstract class TexturedObject extends GameObject {
 
     protected Identifier texture;
+    private boolean modelLoaded;
 
     public TexturedObject(int x, int y, int w, int h, Identifier texture) {
         super(x, y, w, h);
         this.texture = texture;
+        this.modelLoaded = false;
     }
 
     protected int prepareTexture() {
@@ -21,12 +23,13 @@ public abstract class TexturedObject extends GameObject {
 
     @Override
     public void draw() {
-        if (model == null) {
+        if (!modelLoaded) {
             model = InnocentDream.display.loader.loadToVAO(new float[]{
                     aABB.x, aABB.y, 0,
                     aABB.x, aABB.y + aABB.h, 0,
                     aABB.x + aABB.w, aABB.y + aABB.h, 0,
                     aABB.x + aABB.w, aABB.y, 0}, texCoords);
+            modelLoaded = true;
         }
         InnocentDream.display.renderer.renderTexturedModel(this.model, prepareTexture());
     }
