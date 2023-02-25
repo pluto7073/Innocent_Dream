762fa2fbe8eebde1ab5b317b3a9c305a463c5841
Pluto7073 <20planetpluto05@gmail.com>
2023-02-24 21:34:21 -0500

    Attempted fixing re-initialization problem, not fixed yet

--- src/java/io/innocentdream/objects/GUIObject.java
+++ src/java/io/innocentdream/objects/GUIObject.java
@@ -2,28 +2,30 @@ package io.innocentdream.objects;
 
 import io.innocentdream.InnocentDream;
 import io.innocentdream.rendering.Model;
-import io.innocentdream.rendering.TextureHelper;
 import io.innocentdream.utils.Identifier;
 import io.innocentdream.utils.Utils;
 
 public class GUIObject extends TexturedObject {
     private Model m = null;
+    private boolean loaded;
 
     public GUIObject(int x, int y, int w, int h, Identifier texture) {
         super(x, y, w, h, texture);
+        this.loaded = false;
     }
 
     @Override
     public void draw() {
         float[] closeCorner = Utils.convertPixelToScreenCoordinates(new float[] { aABB.x, aABB.y });
         float[] farCorner = Utils.convertPixelToScreenCoordinates(new float[] { aABB.x + aABB.w, aABB.y + aABB.h });
-        if (m == null) {
+        if (!loaded) {
             m = InnocentDream.display.loader.loadToVAO(new float[]{
                     closeCorner[0], closeCorner[1],
                     closeCorner[0], farCorner[1],
                     farCorner[0], farCorner[1],
                     farCorner[0], closeCorner[1]
             });
+            loaded = true;
         }
         InnocentDream.display.renderer.renderGuiObject(m, prepareTexture());
     }
